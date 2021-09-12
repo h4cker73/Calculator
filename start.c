0cd3c508ed8c20eb373f7b1ee58b0548fea00905
Mr MJT <87242348+Mr6MJT@users.noreply.github.com>
2021-09-12 13:22:50 +0300

    Update start.c

--- start.c
+++ start.c
@@ -9,18 +9,22 @@ int main() {
     printf("\033[0;31m ##  This is my first project in C language ## \n");
     printf("\033[0;31m ############################################# \n");
     printf("\n"); 
-    printf("{</>} please enter the first value [x] : ");
-    scanf("%d", &x);
-    printf("{</>} please enter the second value [y] : ");
-    scanf("%d", &y);
-    printf("\n");
-    int add = x + y;
-    int sub = x - y;
-    int mul = x * y;
-    int div = x / y;
-    printf("{</>} x + y = %d\n", add);
-    printf("{</>} x - y = %d\n", sub);
-    printf("{</>} x * y = %d\n", mul);
-    printf("{</>} x / y = %d\n", div);
-    return 0;
+    int xx = 0;
+    while (xx < 2)
+    {
+       printf("\n");
+       printf("{</>} please enter the first value [x] : ");
+       scanf("%d", &x);
+       printf("{</>} please enter the second value [y] : ");
+       scanf("%d", &y);
+       printf("\n");
+       int add = x + y;
+       int sub = x - y;
+       int mul = x * y;
+       int div = x / y;
+       printf("{</>} x + y = %d\n", add);
+       printf("{</>} x - y = %d\n", sub);
+       printf("{</>} x * y = %d\n", mul);
+       printf("{</>} x / y = %d\n", div);
+    }
 }
