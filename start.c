1abde283ccf9744a5b36abf34bd3c2680dac7744
Mr MJT <87242348+Mr6MJT@users.noreply.github.com>
2021-09-18 13:02:03 +0300

    Update start.c

--- start.c
+++ start.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// if you are coming to read the source code i want to tell  you that there is no malware inside the script,continue reading :)
 int main() {
     int x, y;
     system("clear");
@@ -13,18 +13,18 @@ int main() {
     while (xx < 2)
     {
        printf("\n");
-       printf("{</>} please enter the first value [x] : ");
+       printf("-please enter the first value : ");
        scanf("%d", &x);
-       printf("{</>} please enter the second value [y] : ");
+       printf("-please enter the second value : ");
        scanf("%d", &y);
        printf("\n");
        int add = x + y;
        int sub = x - y;
        int mul = x * y;
        int div = x / y;
-       printf("{</>} x + y = %d\n", add);
-       printf("{</>} x - y = %d\n", sub);
-       printf("{</>} x * y = %d\n", mul);
-       printf("{</>} x / y = %d\n", div);
+       printf("%d + %d = %d\n", x, y, add);
+       printf("%d - %d = %d\n", x, y, sub);
+       printf("%d * %d = %d\n", x, y, mul);
+       printf("%d / %d = %d\n", x, y, div);
     }
 }
